e2adc31cc8454116e2e26a2596f6d082ef7fb774
Tim McMackin <tmcmackin@shutterstock.com>
2019-06-05 14:35:09 -0400

    add front matter before Jekyll build

--- build/jekyll/scripts/fix-readme.js
+++ build/jekyll/scripts/fix-readme.js
@@ -9,6 +9,11 @@ const writeFile = promisify(fs.writeFile);
 
 const readmeFilePath = `${__dirname}/../README.md`;
 
+const readmeFrontMatter = `---
+title: Official JavaScript SDK client for the Shutterstock API
+permalink: index
+---`;
+
 // Correct links in README.md
 // from [**createSoundbox**](docs/AudioApi.md#createSoundbox)
 // to [**createSoundbox**](docs/AudioApi.html#createSoundbox)
@@ -34,22 +39,10 @@ async function addAcknowledgement(readmeFilePromise) {
   return readmeText + '\n## Acknowledgements\n\nThanks to Tom Johnson of [I\'d Rather Be Writing](https://idratherbewriting.com/) for this Jekyll template.'
 }
 
-// For Jekyll to process plaintext links into clickable links, add <>.
-// Skip markdown links with urls within parens and brackets and links that are within asterisks.
-async function linkifyUrls(readmeFilePromise) {
-  const simpleUrlRegexAvoidMarkdownLinks = /[^\(*\[](https?:\/\/[\w-.\/]*\w)/gm;
+// Add Jekyll front matter
+async function addFrontMatter(readmeFilePromise) {
   const readmeText = await Promise.resolve(readmeFilePromise);
-  const readmeByLines = readmeText.split('\n');
-  const fixedReadme = _.map(readmeByLines, (line) => {
-    var match;
-    var fixedLine = line;
-    while (match = simpleUrlRegexAvoidMarkdownLinks.exec(line)) {
-      const correctedLink = `<${match[1]}>`;
-      fixedLine = fixedLine.replace(match[1], correctedLink);
-    }
-    return fixedLine;
-  });
-  return fixedReadme.join('\n');
+  return readmeText.replace('# Official JavaScript SDK client for the Shutterstock API', readmeFrontMatter);
 }
 
 async function writeReadmeFile(readmeFilePromise) {
@@ -61,16 +54,16 @@ _.mixin({
   fixReadmeLinks,
   writeReadmeFile,
   addAcknowledgement,
-  linkifyUrls,
   readFile,
+  addFrontMatter,
 });
 
 async function fixReadme() {
   const result = await _.chain(readmeFilePath)
     .readFile('utf8')
     .fixReadmeLinks()
+    .addFrontMatter()
     .addAcknowledgement()
-    .linkifyUrls()
     .writeReadmeFile()
     .value();
 }
